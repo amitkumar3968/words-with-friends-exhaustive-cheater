6c412565dc1c6019fa5993f52a0e5ca800dc45de
lokling@gmail.com <lokling@gmail.com@2165158a-c2d0-8582-d542-857db5896f79>
2011-04-03 10:35:55 +0000

    Dictionary code

---
+++ WordsCheater/trunk/Dictionary.cpp
@@ -0,0 +1,38 @@
+#include "Dictionary.h"
+#include <fstream>
+#include <regex>
+#include <sstream>
+
+Dictionary::Dictionary( const std::string& dictFilePath )
+ :m_dictionaryFile( )
+{
+    std::ifstream ifs( dictFilePath );
+    if( !ifs )
+        throw std::invalid_argument( __FUNCTION__ "invalid dictionary file path " );
+    
+    std::stringstream ss;
+    ss << ifs.rdbuf( );
+    m_dictionaryFile = ss.str();
+
+    ifs.close( );
+}
+
+bool Dictionary::IsStringValid( const std::string& str )
+{
+    if( str.size() < 2 )
+        return false;
+    std::regex ex( std::string("^") + str + "$" );
+    return std::regex_search( m_dictionaryFile.begin(),
+                              m_dictionaryFile.end(),
+                              ex );
+}
+
+bool Dictionary::IsSubStringValid( const std::string& str )
+{
+    if( str.size() < 2 )
+        return false;
+    std::regex ex( str );
+    return std::regex_search( m_dictionaryFile.begin(),
+                              m_dictionaryFile.end(),
+                              ex );
+}
